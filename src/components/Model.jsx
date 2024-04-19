f428ad1e0c84360a092f90d816d6e81186434c9f
grenst <henkshredder@gmail.com>
2024-04-19 15:42:25 +0200

    ADD viewport tab

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,8 +1,22 @@
 import { useGSAP } from '@gsap/react'
 import { gsap } from 'gsap'
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import ModelView from './ModelView'
 
 const Model = () => {
+    const [size, setSize] = useState('small');
+    const [Model, setModel] = useState({
+        title: 'iPhone 15 Pro in Natural Titanium',
+        color: ['#8F8A81', '#FFE7B9', '#6F6C64'],
+        img: yellowImg,
+    })
+
+    //Camera controll for Viewport
+    const cameraControlSmall = useRef();
+    const cameraControlLarge = useRef();
+
+    const small = useRef(new THREE.Group());
+
     useGSAP(() => {
         gsap.to('#heading', { y: 0, opacity: 1})
     }, [])
@@ -13,6 +27,12 @@ const Model = () => {
                 <h1 id="heading" className="section-heading">
                     Take a closer look.
                 </h1>
+
+                <div className='flex flex-col items-center mt-5'>
+                    <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative">
+                        <ModelView />
+                    </div>
+                </div>
             </div>
         </section>
     )
