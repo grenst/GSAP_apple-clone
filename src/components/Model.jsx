7eda7a3412efc1a1ba31cede2bba0a8601c0f5ad
grenst <henkshredder@gmail.com>
2024-04-19 16:39:48 +0200

    ADD color switchers

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -7,6 +7,7 @@ import * as THREE from 'three';
 import { yellowImg } from '../utils';
 import { Canvas } from '@react-three/fiber';
 import { View } from '@react-three/drei';
+import { models } from '../constans';
 
 const Model = () => {
     const [size, setSize] = useState('small');
@@ -75,6 +76,19 @@ const Model = () => {
                         >
                             <View.Port />
                         </Canvas>
+                    </div>
+
+                    <div className="mx-auto w-full">
+                        <p className="text-sm font-light text-center mb-5">{model.title}</p>
+
+                        <div className="flex-center">
+                            <ul className="color-container">
+                                {models.map((item, i) => (
+                                    <li key={i} className="w-6 h-6 rounded-full mx-2" style={{ backgroundColor: item.color[0] }}
+                                    onClick={() => setModel(item)} />
+                                ))}
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
