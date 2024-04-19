928d70abb3854081eeadc3f9449624e3a462bc2c
grenst <henkshredder@gmail.com>
2024-04-19 15:58:41 +0200

    ADD param for model

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -3,9 +3,12 @@ import { gsap } from 'gsap'
 import React, { useRef, useState } from 'react'
 import ModelView from './ModelView'
 
+import * as THREE from 'three';
+import { yellowImg } from '../utils';
+
 const Model = () => {
     const [size, setSize] = useState('small');
-    const [Model, setModel] = useState({
+    const [model, setModel] = useState({
         title: 'iPhone 15 Pro in Natural Titanium',
         color: ['#8F8A81', '#FFE7B9', '#6F6C64'],
         img: yellowImg,
@@ -15,7 +18,13 @@ const Model = () => {
     const cameraControlSmall = useRef();
     const cameraControlLarge = useRef();
 
+    //models 
     const small = useRef(new THREE.Group());
+    const large = useRef(new THREE.Group());
+
+    //rotations
+    const [smallRotation, setSmallRotation] = useState(0);
+    const [largeRotation, setLargeRotation] = useState(0);
 
     useGSAP(() => {
         gsap.to('#heading', { y: 0, opacity: 1})
@@ -30,7 +39,15 @@ const Model = () => {
 
                 <div className='flex flex-col items-center mt-5'>
                     <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative">
-                        <ModelView />
+                        <ModelView 
+                            index={1}
+                            groupRef={small}
+                            gsapType="view1"
+                            controlRef={cameraControlSmall}
+                            setRotationState={setSmallRotation}
+                            item={model}
+                            size={size}
+                        />
                     </div>
                 </div>
             </div>
