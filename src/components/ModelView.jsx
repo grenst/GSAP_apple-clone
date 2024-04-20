72db87f00f744a4b627bca38550c38a62f587cbc
grenst <henkshredder@gmail.com>
2024-04-20 14:21:38 +0200

    ADD OerbitalControl moving of model in Viewport

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,15 +1,17 @@
-import { PerspectiveCamera, View } from "@react-three/drei"
-import Lights from "./Lights"
-import IPhone from "./IPhone"
-import Loader from "./Loader"
-import { Suspense } from "react"
+import { OrbitControls, PerspectiveCamera, View } from "@react-three/drei"
+
+import * as THREE from 'three'
+import Lights from "./Lights";
+import IPhone from "./IPhone";
+import Loader from "./Loader";
+import { Suspense } from "react";
 
 const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationSize, size, item }) => {
   return (
     <View
       index={index}
       id={gsapType}
-      className={`border-2 border-red-500 w-full h-full ${index === 2} ? 'right-[-100%] : ''`}
+      className={`w-full h-full ${index === 2} ? 'right-[-100%] : ''`}
     >
       {/* Ambient light */}
       <ambientLight intensity={0.3} />
@@ -18,13 +20,29 @@ const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationSize, siz
 
       <Lights />
 
-      <Suspense fallback={<Loader />}>
-        <IPhone 
-          scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
-          item={item}
-          size={size}
-        />
-      </Suspense>
+      <OrbitControls
+        makeDefault
+        ref={controlRef}
+        enableZoom={false}
+        enablePan={false}
+        rotateSpeed={0.4}
+        target={new THREE.Vector3(0, 0, 0)}
+        onEnd={() => setRotationState(controlRef.current.getAzimuthalAngle())}
+      /> 
+
+      <group
+        ref={groupRef}
+        name={`${index === 1} ? 'small' : 'large'`}
+        position={[0, 0, 0]}>
+
+        <Suspense fallback={<Loader />}>
+          <IPhone 
+            scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
+            item={item}
+            size={size}
+          />
+        </Suspense>
+      </group>
     </View>
   )
 }
